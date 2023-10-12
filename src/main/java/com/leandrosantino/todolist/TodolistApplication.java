26cfb32c7037a8b03ae8409a1b8ee184fef06589
leandrosantino <leandrosantino2013@gmail.com>
2023-10-12 17:22:58 -0300

    initial commit

---
+++ src/main/java/com/leandrosantino/todolist/TodolistApplication.java
@@ -0,0 +1,15 @@
+package com.leandrosantino.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistApplication {
+
+	public static void main(String[] args){
+		SpringApplication.run(TodolistApplication.class, args);
+		System.out.println("\033[H\033[2J");
+		System.out.println("Server Online!! \n");
+	}
+
+}
---
+++ src/main/java/com/leandrosantino/todolist/task/TaskController.java
@@ -0,0 +1,76 @@
+package com.leandrosantino.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.leandrosantino.todolist.utils.Utils;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        UUID idUser = (UUID) request.getAttribute("idUser");
+        taskModel.setIdUser(idUser);
+
+        var currentDate = LocalDateTime.now();
+
+        if(currentDate.isAfter(taskModel.getStarAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("curent date is after of start date ");
+        }
+
+        if(currentDate.isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("curent date is after of end date ");
+        }
+
+        if(taskModel.getStarAt().isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("start date is after of end date ");
+        }
+
+
+
+        var task = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping
+    public List<TaskModel> list(HttpServletRequest request){
+        var idUser = (UUID) request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser(idUser);
+
+        return tasks;
+    }
+
+    @PutMapping("/{id}")
+    public TaskModel update(
+        @RequestBody TaskModel taskModel,
+        @PathVariable UUID id,
+        HttpServletRequest request
+    ){
+        var task = this.taskRepository.findById(id).orElse(null);
+        Utils.copyNonNullProperties(taskModel, task);
+        return this.taskRepository.save(task);
+    }
+
+}
